dbbd95b7568fc2310705a33cd0376cceaea39d00
Vinicius Gabriel <163018302+breguenice3@users.noreply.github.com>
2024-04-23 19:14:25 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Bio from './components/bio/bio';
+import Cards from './components/cards/cards';
 import Header from './components/header/header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Bio />
+      <Cards />
     </div>
   );
 }
--- src/components/bio/bio.tsx
+++ src/components/bio/bio.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react"
 import styled from "styled-components";
-import { RiReactjsFill } from "react-icons/ri";
-import { FaHtml5, FaCss3Alt } from "react-icons/fa";
-import { DiJavascript } from "react-icons/di";
-
+import { RiReactjsFill, RiJavascriptLine } from "react-icons/ri";
+import { RiHtml5Line } from "react-icons/ri";
+import { FaCss3Alt } from "react-icons/fa";
+import { TbBrandTypescript } from "react-icons/tb";
+import { useEffect, useState } from "react";
 
 const Div = styled.div`
 display: flex;
@@ -16,10 +16,13 @@ padding: 200px 0px;
 & img{
     width: 400px;
     border-radius: 50%;
+    box-shadow: 20px 20px 20px 10px #0d022189;
 }
 
 & img:hover{
     transform: scale(1.1);
+    transition: ease 0.4s;
+    cursor: pointer;
 }
 `
 const Info = styled.div`
@@ -27,55 +30,76 @@ display: flex;
 flex-direction: column;
 height: 100%;
 width: 500px;
-gap: 10px;
 padding: 100px 0px;
 font-family: "Roboto", sans-serif;
 
 & p{
     font-size: 20px;
+    padding: 0px 0px 20px 0px;
 }
 
 & h3{
-    padding-top: 10px;
+    padding: 10px 0px 10px 0px;
     font-size: 24px;
 }
 
 & h1{
     font-size: 34px;
 }
+
+& h2{
+    font-size: 16px;
+    padding: 5px 0px 20px 0px;
+}
+
+& a{
+    text-decoration: none;
+}
+
+& a:hover{
+    color: #0000003e;
+}
+`
+
+const Icons = styled.div`
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    height: auto;
 `
 
 export default function Bio() {
 
-    const [perfil, setPerfil] = useState<any>([])
+    const [bio, setBio] = useState<any>([])
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch('https://api.github.com/users/breguenice3/');
+            const response = await fetch(`https://api.github.com/users/breguenice3`);
             const data = await response.json();
-            setPerfil(data);
+            setBio(data);
         }
         fetchData();
     }, []);
-    console.log(perfil);
 
     return (
         <div>
             <Div>
                 <div>
-                    <img src={perfil.avatar_url} alt="avatar de breguenice" />
+                    <img src={bio.avatar_url} alt="avatar de breguenice" />
                 </div>
                 <Info>
-                    <h1>{perfil.name}</h1>
-                    <h3>{perfil.location}</h3>
-                    <p>{perfil.bio}</p>
+                    <h1>{bio.name}</h1>
+                    <h2><a href="https://github.com/breguenice3" target="_blank">@{bio.login}</a></h2>
+                    <h3>{bio.location}</h3>
+                    <p>{bio.bio}</p>
 
-                    <div>
-                        <FaHtml5 />
-                        <FaCss3Alt />
-                        <DiJavascript />
-                        <RiReactjsFill />
-                    </div>
+                    <Icons>
+                        <RiHtml5Line size={35} />
+                        <FaCss3Alt size={35} />
+                        <RiJavascriptLine size={40} />
+                        <TbBrandTypescript size={36} />
+                        <RiReactjsFill size={36} />
+                    </Icons>
                 </Info>
             </Div>
         </div>
